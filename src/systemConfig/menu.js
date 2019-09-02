341436dfe17429161b0e5a5a8cb27d7ac82f2a47
lxzi <1084450116@qq.com>
2019-09-02 14:59:33 +0800

    初始化，清除多余代码

--- src/systemConfig/menu.js
+++ src/systemConfig/menu.js
@@ -4,168 +4,28 @@ let menu = [
     childList: [
       {
         active: false,
-        name: '版本管理',
-        url: '/scripts-group',
+        name: '测试菜单',
+        url: '',          //菜单路径，文件夹
         type: -1,
         image: '',
         tag: '',
         meta: {
           routeType: 'outer',
-          title: '版本管理'
-        },
-        childList: []
+          title: '测试菜单'
+        }
       }
     ],
     collapsed: false,
-    name: '版本配置管理',
+    name: '测试菜单',
     url: '/',
     image: '',
     static: true,
-    title: '版本配置管理'
-  },
-  {
-    active: false,
-    childList: [
-      {
-        active: false,
-        name: '数据库结构管理',
-        url: '/scripts-management',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '数据库结构管理'
-        },
-        childList: []
-      },
-      {
-        active: false,
-        name: '数据版本管理',
-        url: '/monitoring-data',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '数据版本管理'
-        },
-        childList: []
-      },
-      {
-        active: false,
-        name: '多环境数据库结构对比',
-        url: '/structural-comparison',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '多环境数据库结构对比'
-        },
-        childList: []
-      },
-      {
-        active: false,
-        name: '导出数据库结构',
-        url: '/export-database',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '导出数据库结构'
-        },
-        childList: []
-      },
-      {
-        active: false,
-        name: '导出数据库记录',
-        url: '/export-records',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '导出数据库记录'
-        },
-        childList: []
-      },
-      {
-        active: false,
-        name: '导入数据库记录',
-        url: '/import-records',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '导入数据库记录'
-        },
-        childList: []
-      }
-    ],
-    collapsed: false,
-    name: '数据库脚本管理',
-    url: '/',
-    image: '',
-    static: true,
-    title: '数据库脚本管理'
-  },
-  {
-    active: false,
-    childList: [
-      {
-        active: false,
-        name: '标准术语导入',
-        url: '/terminology-import',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '标准术语导入'
-        },
-        childList: []
-      },
-      {
-        active: false,
-        name: '后结构化-诊断正则导入',
-        url: '/regular-import',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '后结构化-诊断正则导入'
-        },
-        childList: []
-      },
-      {
-        active: false,
-        name: '同义词导入',
-        url: '/synonym-import',
-        type: -1,
-        image: '',
-        tag: '',
-        meta: {
-          routeType: 'outer',
-          title: '同义词导入'
-        },
-        childList: []
-      }
-    ],
-    collapsed: false,
-    name: '数据版本管理',
-    url: '/',
-    image: '',
-    static: true,
-    title: '数据版本管理'
+    title: '测试'
   }
 ]
 // 注意，如果要在tab显示菜单，对应的路由地址要设置
 // meta: {
 //       routeType: 'outer',
 // }
-
+// let menu = []
 export default menu
